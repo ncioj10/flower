a5089acdf4ef2fa2663568ab1ee2f8c6051660b6
Nicolas Jakob <jakob@veios.com>
2021-11-07 14:51:42 +0100

    logging and robustness

--- src/py/flwr/client/grpc_client/connection.py
+++ src/py/flwr/client/grpc_client/connection.py
@@ -68,7 +68,7 @@ def insecure_grpc_connection(
         ],
     )
     channel.subscribe(on_channel_state_change)
-
+    log(DEBUG, "Subscribed")
     queue: Queue[ClientMessage] = Queue(  # pylint: disable=unsubscriptable-object
         maxsize=1
     )
